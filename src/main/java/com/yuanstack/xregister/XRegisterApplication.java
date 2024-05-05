1b6185ab7dbdadc858f57fa43cc5caa9a8b325ad
Hansiyuan131 <422295068@qq.com>
2024-05-05 21:08:11 +0800

    feat: init

---
+++ src/main/java/com/yuanstack/xregister/XRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.yuanstack.xregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XRegisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuanstack/xregister/XRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuanstack.xregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
